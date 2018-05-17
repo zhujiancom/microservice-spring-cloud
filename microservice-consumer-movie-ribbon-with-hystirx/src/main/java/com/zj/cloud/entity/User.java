613212cd71febb3b927086f0cbadb4c7e5191d31
zhujiancom <eric87com@gmail.com>
2018-05-18 07:42:14 +0800

    hystrix

---
+++ microservice-consumer-movie-ribbon-with-hystirx/src/main/java/com/zj/cloud/entity/User.java
@@ -0,0 +1,58 @@
+package com.zj.cloud.entity;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by zhuji on 4/24/2018.
+ */
+public class User {
+    private long id;
+
+    private String username;
+
+    private String name;
+
+    private int age;
+
+    private BigDecimal balance;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
