0173cb8ca2030eae38958b491751c1258d6eb93a
zhujiancom <eric87com@gmail.com>
2018-05-21 18:23:22 +0800

    hystrix dashboard

--- microservice-consumer-movie-ribbon-with-hystrix/src/main/java/com/zj/cloud/MicroserviceConsumerMovieHystrixApplication.java
+++ microservice-consumer-movie-ribbon-with-hystrix/src/main/java/com/zj/cloud/MicroserviceConsumerMovieHystrixApplication.java
@@ -1,18 +1,14 @@
 package com.zj.cloud;
 
-import com.zj.cloud.config.MyRibbonClientConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@ComponentScan(excludeFilters = {@ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, value=MyRibbonClientConfiguration.class)})
 @EnableEurekaClient
 @EnableCircuitBreaker
 public class MicroserviceConsumerMovieHystrixApplication {
---
+++ microservice-hystrix-turbine/Readme.md
@@ -0,0 +1,2 @@
+Turnbine:
+hystrix.stream 的集群信息展示
